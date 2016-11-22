026fa55bd8e92d5ab6b7de7377b6d2e9b783c8a2
Sonia Arai <soninhanaomi@gmail.com>
2016-11-22 14:26:54 -0200

    add a colunm and btn bootstrap

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ApplicationRecord
   validates :description, presence:true
+
+  def mark_as_done
+    self.done = true
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161122132533) do
+ActiveRecord::Schema.define(version: 20161122154619) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "done"
   end
 
 end
